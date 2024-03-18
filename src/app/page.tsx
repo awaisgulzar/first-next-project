65f4728f92c27246de3e5616b9cb4b289d95ccae
“[awaisgulzar]” <[mawaisjani78563@gmail.com]>
2024-03-18 12:33:13 +0500

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
 function HomePage() {
-  return <h1>Hello, I am [Your Name] and this is my first Next.js project.</h1>
+  return <h1>Hello, I am Muhammad Awais and this is my first Next.js project.</h1>
 }
 export default HomePage;
