ab0a525174977125610ee84b69012e2e20ccc974
“[awaisgulzar]” <[mawaisjani78563@gmail.com]>
2024-03-30 16:28:05 +0500

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import Link from 'next/link'
 
-function HomePage() {
+function homePage() {
   return (
     <main  >
       <Link  className='overline'  href="/name">go to name</Link>
     </main>
   )
 }
-export default HomePage; 
+export default homePage; 
